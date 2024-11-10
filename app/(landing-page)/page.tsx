4e82a0b54bae48cb05389c19d140a60e2e5e5b68
Charan <charanxmn@gmail.com>
2024-11-10 17:57:15 +0530

    Updated the text color of the hero paragraph.

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className=" mx-auto text-center h-[calc(100vh-69px)] flex items-center justify-center flex-col">
         <h1 className="italic text-6xl font-extrabold">Hyper ⚡️tudy Force</h1>{" "}
         {/* Add a Full text logo here */}
-        <p className="text-3xl max-w-[600px] mt-8">
+        <p className="text-3xl max-w-[600px] mt-8 text-muted-foreground">
           Study topics of your choice by chatting with AI directly or by using
           your own PDF books and notes
         </p>
